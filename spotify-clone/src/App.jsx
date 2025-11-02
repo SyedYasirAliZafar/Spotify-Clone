ea510ef9f12e1a2cfe16e46f6b22aca467071633
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2025-11-03 02:16:17 +0500

    Ui Created

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
 
 function App() {
 
-
   return (
     <>
-      <div className='bg-zinc-900 text-white'>Hello</div>
+      <div className='bg-black h-screen '>
+        <div className='h-[90%] flex'>
+          <Sidebar/>
+          <Display/>
+        </div>
+        <Player/>
+      </div>
     </>
   )
 }
---
+++ spotify-clone/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+function Navbar() {
+  return (
+    <>
+        <div className='w-full flex justify-between items-center font-semibold'>
+            <div className='flex items-center gap-2'>
+                <img className='w-8 bg-black rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
+                <img className='w-8 bg-black rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
+            </div>
+            <div className='flex items-center gap-4'>
+                <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer active:scale-95'>Explore Premium</p>
+                <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer active:scale-90 '>Install App</p>
+                <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>Y</p>
+            </div>
+        </div>
+        <div className='flex items-center gap-2 mt-4'>
+            <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer active:scale-95'>All</p>
+            <p className='bg-zinc-800 px-4 py-1 rounded-2xl cursor-pointer active:scale-95'>Music</p>
+            <p className='bg-zinc-800 px-4 py-1 rounded-2xl cursor-pointer active:scale-95'>Podcast</p>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
